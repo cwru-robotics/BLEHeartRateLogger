2976cdae3465bb73d8671c8324706b6dcb5c3ea0
Tom <TomShkurti.school@gmail.com>
2020-01-24 15:05:23 -0500

    Last working state where HRV is measured and printed.
    Note: Communication with hardware is crashing randomly.

--- BLEHeartRateLogger.py
+++ BLEHeartRateLogger.py
@@ -241,7 +241,7 @@ def main(addr=None, sqlfile=None, gatttool="gatttool", check_battery=False, hr_h
 
             while 1:
                 try:
-                    gt.expect(r"handle: (0x[0-9a-f]+), uuid: ([0-9a-f]{8})", timeout=10)
+                    gt.expect(r"handle: (0x[0-9a-f]+), uuid: ([0-9a-f]{8})", timeout=30)
                 except pexpect.TIMEOUT:
                     break
                 handle = gt.match.group(1)
@@ -306,6 +306,7 @@ def main(addr=None, sqlfile=None, gatttool="gatttool", check_battery=False, hr_h
 
             if sqlfile is None:
                 log.info("Heart rate: " + str(res["hr"]))
+                log.info("RR interval: " + str(res["rr"]))
                 continue
 
             # Push the data to the database
